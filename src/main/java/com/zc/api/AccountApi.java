8329e4e181893008b53a7a8de2a5d6946452d4b7
zhenqiang <zhangzq@heptax.com>
2016-10-14 19:11:51 +0800

    调整好登录接口

--- src/main/java/com/zc/api/AccountApi.java
+++ src/main/java/com/zc/api/AccountApi.java
@@ -3,16 +3,16 @@ package com.zc.api;
 import com.zc.enumeration.StatusCodeEnum;
 import com.zc.model.usermodel.LoginModel;
 import com.zc.model.usermodel.LoginStatus;
-import com.zc.shiro.LoginService;
+import com.zc.service.UsersService;
 import com.zc.utility.ValidateHelper;
 import com.zc.utility.response.ApiResultModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.Map;
@@ -20,12 +20,12 @@ import java.util.Map;
 /**
  * Created by 张镇强 on 2016/10/14 16:24.
  */
-@Controller
+@RestController
 @RequestMapping("/api/account/")
 public class AccountApi extends BaseApi {
     private final Logger logger = LoggerFactory.getLogger(AccountApi.class);
     @Autowired
-    LoginService loginService;
+    private UsersService usersService;
 
     @RequestMapping(value = "login", method = RequestMethod.POST)
     public ApiResultModel login(@Valid LoginModel loginModel,
@@ -39,11 +39,12 @@ public class AccountApi extends BaseApi {
             return result;
         }
 
-        LoginStatus loginStatus = loginService.login(loginModel.getUsername(),
+        LoginStatus loginStatus = usersService.login(loginModel.getUsername(),
                 loginModel.getPassword());
         if (!loginStatus.isLoggedIn()) {
             logger.info("用户{}登录失败", loginModel.getUsername());
             result.setStatusCode(StatusCodeEnum.FAILED);
+            return result;
         }
 
         logger.info("用户{}登录成功", loginModel.getUsername());
---
+++ src/test/com/zc/service/UserServiceTest.java
@@ -0,0 +1,30 @@
+package com.zc.service;
+
+import com.zc.BaseTest;
+import com.zc.bean.Users;
+import com.zc.model.usermodel.LoginStatus;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created by 张镇强 on 2016/10/14 18:29.
+ */
+public class UserServiceTest extends BaseTest {
+    @Autowired
+    private UsersService usersService;
+
+    @Test
+    public void addUserTest() {
+        Users users = new Users();
+        users.setUserName("heptax");
+        users.setPassword("Zhicang@2016");
+        Assert.assertTrue(usersService.add(users));
+    }
+
+    @Test
+    public void loginTest() {
+//        LoginStatus loginStatus = usersService.login("heptax", "Zhicang@2016");
+//        Assert.assertTrue(loginStatus.isLoggedIn());
+    }
+}
