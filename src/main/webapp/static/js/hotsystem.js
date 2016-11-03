59a8ae4d95f72cc98e553f5a531a6e0525b0af04
zhujm <zhujm@heptax.com>
2016-11-03 17:40:34 +0800

    删除注释代码

--- src/main/webapp/static/js/hotsystem.js
+++ src/main/webapp/static/js/hotsystem.js
@@ -133,8 +133,7 @@
 		$('#ser_dialog').addClass('hidecommon');
 	});
 	
-	$('.dialog_tab li').on('click',function(){
-		
+	$('.dialog_tab li').on('click',function(){		
 		var $t = $(this).index();
 		var $ul = $(this).parent().next().find('ul');
 		if($ul.eq($t).css('display')=='block'){
@@ -150,18 +149,6 @@
 			$(this).addClass('hot_arrow_up');
 		}
 	});
-	//滚动条
-//	
-//	$('.inp_ch_list').niceScroll({
-//        cursorcolor: "#ccc",//#CC0071 光标颜色
-//        cursoropacitymax: 1, //改变不透明度非常光标处于活动状态（scrollabar“可见”状态），范围从1到0
-//        touchbehavior: false, //使光标拖动滚动像在台式电脑触摸设备
-//        cursorwidth: "5px", //像素光标的宽度
-//        cursorborder: "0", // 	游标边框css定义
-//        cursorborderradius: "5px",//以像素为光标边界半径
-//        autohidemode: false //是否隐藏滚动条
-//    });
-
 	
 //曲线。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。
 var paper;
