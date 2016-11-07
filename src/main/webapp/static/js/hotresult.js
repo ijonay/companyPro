d8f0e14205c2a89142090c4ef05faf066f835533
yusha <yusha@heptax.com>
2016-11-07 12:08:31 +0800

    探索结果页大体布局

---
+++ src/main/webapp/static/js/hotresult.js
@@ -0,0 +1,333 @@
+var result={
+        "data": [{
+            "id": 1,
+            "title": "#霍建华林心如结婚#",
+            "score": 0.44,
+            "coordinate": null,
+            "logoUrl": null,
+            "readNum": 70,
+            "readNumTrendGrowth": null,
+            "relationDesc": null,
+            "topicUrl": null,
+            "topicType": null
+        },{
+            "id": 2,
+            "title": "#王宝强离婚#",
+            "score": 0.44,
+            "coordinate": null,
+            "logoUrl": null,
+            "readNum": 85,
+            "readNumTrendGrowth": null,
+            "relationDesc": null,
+            "topicUrl": null,
+            "topicType": null
+        }
+        ,{
+            "id": 2,
+            "title": "#热点名称热点名称热点名称热点名称热点名称热点名称#",
+            "score": 0.44,
+            "coordinate": null,
+            "logoUrl": null,
+            "readNum": 95,
+            "readNumTrendGrowth": null,
+            "relationDesc": null,
+            "topicUrl": null,
+            "topicType": null
+        },{
+            "id": 2,
+            "title": "#热点名称#",
+            "score": 0.44,
+            "coordinate": null,
+            "logoUrl": null,
+            "readNum": 65,
+            "readNumTrendGrowth": null,
+            "relationDesc": null,
+            "topicUrl": null,
+            "topicType": null
+        },{
+            "id": 2,
+            "title": "#热点名称#",
+            "score": 0.44,
+            "coordinate": null,
+            "logoUrl": null,
+            "readNum": 75,
+            "readNumTrendGrowth": null,
+            "relationDesc": null,
+            "topicUrl": null,
+            "topicType": null
+        },{
+            "id": 2,
+            "title": "#热点名称#",
+            "score": 0.44,
+            "coordinate": null,
+            "logoUrl": null,
+            "readNum": 82,
+            "readNumTrendGrowth": null,
+            "relationDesc": null,
+            "topicUrl": null,
+            "topicType": null
+        },{
+            "id": 2,
+            "title": "#热点名称#",
+            "score": 0.44,
+            "coordinate": null,
+            "logoUrl": null,
+            "readNum": 79,
+            "readNumTrendGrowth": null,
+            "relationDesc": null,
+            "topicUrl": null,
+            "topicType": null
+        },{
+            "id": 2,
+            "title": "#热点名称#",
+            "score": 0.44,
+            "coordinate": null,
+            "logoUrl": null,
+            "readNum": 15,
+            "readNumTrendGrowth": null,
+            "relationDesc": null,
+            "topicUrl": null,
+            "topicType": null
+        },{
+            "id": 2,
+            "title": "#热点名称#",
+            "score": 0.44,
+            "coordinate": null,
+            "logoUrl": null,
+            "readNum": 35,
+            "readNumTrendGrowth": null,
+            "relationDesc": null,
+            "topicUrl": null,
+            "topicType": null
+        },{
+            "id": 2,
+            "title": "#热点名称#",
+            "score": 0.44,
+            "coordinate": null,
+            "logoUrl": null,
+            "readNum": 10,
+            "readNumTrendGrowth": null,
+            "relationDesc": null,
+            "topicUrl": null,
+            "topicType": null
+        },{
+            "id": 1,
+            "title": "#霍建华林心如结婚#",
+            "score": 0.44,
+            "coordinate": null,
+            "logoUrl": null,
+            "readNum": 70,
+            "readNumTrendGrowth": null,
+            "relationDesc": null,
+            "topicUrl": null,
+            "topicType": null
+        },{
+            "id": 2,
+            "title": "#王宝强离婚#",
+            "score": 0.44,
+            "coordinate": null,
+            "logoUrl": null,
+            "readNum": 85,
+            "readNumTrendGrowth": null,
+            "relationDesc": null,
+            "topicUrl": null,
+            "topicType": null
+        }
+        ,{
+            "id": 2,
+            "title": "#热点名称#",
+            "score": 0.44,
+            "coordinate": null,
+            "logoUrl": null,
+            "readNum": 95,
+            "readNumTrendGrowth": null,
+            "relationDesc": null,
+            "topicUrl": null,
+            "topicType": null
+        },{
+            "id": 2,
+            "title": "#热点名称#",
+            "score": 0.44,
+            "coordinate": null,
+            "logoUrl": null,
+            "readNum": 65,
+            "readNumTrendGrowth": null,
+            "relationDesc": null,
+            "topicUrl": null,
+            "topicType": null
+        },{
+            "id": 2,
+            "title": "#热点名称#",
+            "score": 0.44,
+            "coordinate": null,
+            "logoUrl": null,
+            "readNum": 75,
+            "readNumTrendGrowth": null,
+            "relationDesc": null,
+            "topicUrl": null,
+            "topicType": null
+        },{
+            "id": 2,
+            "title": "#热点名称#",
+            "score": 0.44,
+            "coordinate": null,
+            "logoUrl": null,
+            "readNum": 82,
+            "readNumTrendGrowth": null,
+            "relationDesc": null,
+            "topicUrl": null,
+            "topicType": null
+        },{
+            "id": 2,
+            "title": "#热点名称#",
+            "score": 0.44,
+            "coordinate": null,
+            "logoUrl": null,
+            "readNum": 79,
+            "readNumTrendGrowth": null,
+            "relationDesc": null,
+            "topicUrl": null,
+            "topicType": null
+        },{
+            "id": 2,
+            "title": "#热点名称#",
+            "score": 0.44,
+            "coordinate": null,
+            "logoUrl": null,
+            "readNum": 15,
+            "readNumTrendGrowth": null,
+            "relationDesc": null,
+            "topicUrl": null,
+            "topicType": null
+        },{
+            "id": 2,
+            "title": "#热点名称#",
+            "score": 0.44,
+            "coordinate": null,
+            "logoUrl": null,
+            "readNum": 35,
+            "readNumTrendGrowth": null,
+            "relationDesc": null,
+            "topicUrl": null,
+            "topicType": null
+        },{
+            "id": 2,
+            "title": "#热点名称#",
+            "score": 0.44,
+            "coordinate": null,
+            "logoUrl": null,
+            "readNum": 10,
+            "readNumTrendGrowth": null,
+            "relationDesc": null,
+            "topicUrl": null,
+            "topicType": null
+        }],
+        "error": {
+            "code": 0,
+            "message": "成功"
+        }
+    }
+$("<input type='text' class='word word-ellipsis wordwidth' value='关键词'>").appendTo($("#canvas"));
+drawWord(result.data);
+function drawWord(data) {
+    var pointArr = [];
+    $.each(data, function(idx, item) {
+        var r = 133 + idx * 20;
+        var fontClass = "fontClass1",
+            sizeClass = "sizeClass1";
+        if(item.readNum<= 25) {
+            fontClass = "fontClass1";
+            sizeClass = "sizeClass1";
+        } else if(item.readNum<= 50) {
+            fontClass = "fontClass2";
+            sizeClass = "sizeClass2";
+        } else if(item.readNum<= 75) {
+            fontClass = "fontClass3";
+            sizeClass = "sizeClass3";
+        } else if(item.readNum<= 100) {
+            fontClass = "fontClass4";
+            sizeClass = "sizeClass4";
+        }
+        $item = $("<div class='topic'>" +
+                "<span class='icon " + sizeClass + "'>"+item.readNum+"</span><span class='link " + fontClass + "'>" + item.title + "</span></div>").appendTo($("#canvas"));
+        var itemWidth = $item.width();
+        var itemHeight = $item.height();
+        if(itemWidth > 180) {
+            itemWidth = 180;
+            $item.find(".link").css({
+                "display": "block",
+                "width": "180px"
+            }).addClass("word-ellipsis");
+        }
+        var itemPos = checkao(idx, r, itemWidth, itemHeight, pointArr);
+        pointArr.push({
+            height: itemHeight,
+            width: itemWidth,
+            left: itemPos.left,
+            top: itemPos.top
+        });
+        $item.css({
+            "left": itemPos.left,
+            "top": itemPos.top
+        }).animate({
+            "opacity": 1
+        }, 300 + idx * 100);
+    });
+}
+/*获得显示位置*/
+function checkao(idx, r, itemWidth, itemHeight, pointArr) {
+    var ao = 0;
+    var elem = {
+        width: itemWidth,
+        height: itemHeight,
+        left: 0,
+        top: 0
+    };
+    do {
+        if(idx % 2 == 0) {
+            ao = (Math.floor(Math.random() * 10) % 2 == 0) ? Math.random() * (90 - 0 + 1) + 0 : Math.random() * (360 - 270 + 1) + 270;
+        } else {
+            ao = Math.random() * (270 - 90 + 1) + 90;
+        }
+        elem.left = 494.5 + r * Math.cos(ao * 3.14 / 180) - itemWidth / 2;
+        elem.top = 210 + r * Math.sin(ao * 3.14 / 180) - itemHeight / 2;
+    } while (hitTest(elem, pointArr) || elem.top + itemHeight > 420 || elem.top < 0)
+    return elem;
+}
+/*检查重叠*/
+function hitTest(elem, pointArr) {
+    var overlapping = function(a, b) {
+        if(Math.abs(2 * a.left + a.width - 2 * b.left - b.width) < a.width + b.width) {
+            if(Math.abs(2 * a.top + a.height - 2 * b.top - b.height) < a.height + b.height) {
+                return true;
+            }
+        }
+        return false;
+    };
+    var i = 0;
+    for(i = 0; i < pointArr.length; i++) {
+        if(overlapping(elem, pointArr[i])) {
+            return true;
+        }
+    }
+    return false;
+};
+
+//hover事件
+$(document).delegate('.topic','mouseover',function(){
+//    $(this).css('margin-left',-5+'px')
+//    var fsEle = parseInt($(this).find('.link').css("font-size")); 
+//    $(this).find('.link').css('font-size',fsEle+3 +'px');
+//    var wdEle = parseInt($(this).find('.icon').width()); 
+//    $(this).find('.icon').css('width',wdEle+5 +'px')
+//    $(this).find('.icon').css('height',wdEle+5 +'px')
+});
+
+$(document).delegate('.topic','mouseout',function(){
+//    $(this).css('margin-left',0+'px')
+//    var fsEle = parseInt($(this).find('.link').css("font-size")); 
+//    $(this).find('.link').css('font-size',fsEle-3 +'px');
+//    var wdEle = parseInt($(this).find('.icon').width()); 
+//    $(this).find('.icon').css('width',wdEle-5 +'px')
+//    $(this).find('.icon').css('height',wdEle-5 +'px')
+});
