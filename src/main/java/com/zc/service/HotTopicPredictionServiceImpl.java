b6ee0ffcddc673eccb54171a55f64f9f9d3f99c2
张成礼 <zhangcl@heptax.com>
2016-11-10 20:29:11 +0800

    修改热点预告接口，跨月截取第一天和最后一天

--- src/main/java/com/zc/service/HotTopicPredictionServiceImpl.java
+++ src/main/java/com/zc/service/HotTopicPredictionServiceImpl.java
@@ -1,5 +1,10 @@
 package com.zc.service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import com.zc.model.HotTopicPredictionModel;
@@ -38,6 +43,40 @@ public class HotTopicPredictionServiceImpl implements HotTopicPredictionService 
 
     @Override
     public List<HotTopicPredictionModel> getHotTopicEventList(String dateStr) {
-        return hotTopicPredictionMapper.getHotTopicEventList(dateStr);
+        List<HotTopicPredictionModel> hotTopicEventList = hotTopicPredictionMapper.getHotTopicEventList(dateStr);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            Date currentDate = sdf.parse(dateStr);
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(currentDate);
+            calendar.set(Calendar.DAY_OF_MONTH, calendar.getActualMaximum(Calendar.DAY_OF_MONTH));
+            Date lastDayOfMonth = calendar.getTime();
+
+            calendar.setTime(currentDate);
+            calendar.set(Calendar.DAY_OF_MONTH, calendar.getActualMinimum(Calendar.DAY_OF_MONTH));
+            Date firstDayOfMonth = calendar.getTime();
+
+            for(int i=0;i < hotTopicEventList.size(); i++){
+                HotTopicPredictionModel event = hotTopicEventList.get(i);
+                String startDateStr =event.getStartDate();
+                Date eventStartDate = sdf.parse(startDateStr);
+                String endDateStr = event.getEndDate();
+                Date eventEndDate = sdf.parse(endDateStr);
+                if( eventStartDate.before(firstDayOfMonth) &&
+                        eventEndDate.after( lastDayOfMonth )
+                        ){
+                    event.setStartDate( sdf.format(firstDayOfMonth) );
+                    event.setEndDate( sdf.format(lastDayOfMonth) );
+                }else if( eventStartDate.before( firstDayOfMonth ) && eventEndDate.before( lastDayOfMonth ) ){
+                    event.setStartDate( sdf.format(firstDayOfMonth) );
+                }else if( eventStartDate.before(lastDayOfMonth) && eventEndDate.after( lastDayOfMonth )){
+                    event.setEndDate( sdf.format(lastDayOfMonth) );
+                }
+            }
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return hotTopicEventList;
     }
 }
