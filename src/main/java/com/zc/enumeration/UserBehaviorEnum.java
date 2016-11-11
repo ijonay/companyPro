dfbb1a22e21a5fb6830cbc8a7c4c0ee74af042f5
zhuzhzh <zhuzz@heptax.com>
2016-11-11 16:26:05 +0800

    临时提交

--- src/main/java/com/zc/enumeration/UserBehaviorEnum.java
+++ src/main/java/com/zc/enumeration/UserBehaviorEnum.java
@@ -1,13 +1,22 @@
 package com.zc.enumeration;
 
 /**
+ * 用户行为枚举
  * Created by xyzhuzhou on 2016/11/11 0011 14:01:37.
  */
 public enum UserBehaviorEnum {
 
 
+    /**
+     * 登录
+     * zhuzhzh 2016年11月11日16:11:32
+     */
     Login(1),
 
+    /**
+     * 登出
+     * zhuzhzh 2016年11月11日16:12:01
+     */
     LoginOut(2);
 
 
